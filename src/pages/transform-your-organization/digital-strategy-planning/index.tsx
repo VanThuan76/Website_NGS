734f81c5a8c481cf49ee81efa675eec7c8794db5
trinhthuy4801 <trinhthuytt118@gmail.com>
2023-09-13 17:34:06 +0700

    TYO Thủy 13/09
    3.1 3.2 một chút 3.3

--- src/pages/transform-your-organization/digital-strategy-planning/index.tsx
+++ src/pages/transform-your-organization/digital-strategy-planning/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import LayoutWebsite from 'src/shared/components/layout/LayoutWebsite';
 import ChallengeDesignOneSection from '@/components/business/website/TYO&SYB_Common/Challenge_DesignOne_Section';
-import { TYOchallenge1 } from '@/mocks/website/TYO/overview';
+import { TYO_DSP_Section3, TYO_DSP_Section4 } from '@/mocks/website/TYO/overview';
+import ServiceDesignThreeSection from '@/components/business/website/TYO&SYB_Common/Service_DesignThree_Section';
+import BannerDesignOneSection from '@/components/business/website/All_Common/Banner_DesignOne_Section';
+import { TYO_DSP_Banner } from '@/mocks/website/TYO/new';
 
-const DTPPage = () => {
+
+const DSPPage = () => {
   return (
     <React.Fragment>
       <Head>
-        <title>YNG_ERP NGS</title>
-        <meta name='description' content='YNG_ERP NGS' />
+        <title>Digital Strategy Planning NGS</title>
+        <meta name='description' content='Digital Strategy Planning NGS' />
         <meta name='keywords' content='Công nghệ thông tin, Giải pháp số' />
       </Head>
-      <ChallengeDesignOneSection title='Thách thức' data={TYOchallenge1} />
+      <BannerDesignOneSection title="Chuyển đổi số" data={TYO_DSP_Banner}/>
+      <ChallengeDesignOneSection title='Thách thức' data={TYO_DSP_Section3} />
+      <ServiceDesignThreeSection title='Dịch vụ' data={TYO_DSP_Section4} />
     </React.Fragment>
   );
 };
 
-DTPPage.getLayout = (children: React.ReactNode) => <LayoutWebsite>{children}</LayoutWebsite>;
-export default DTPPage;
+DSPPage.getLayout = (children: React.ReactNode) => <LayoutWebsite>{children}</LayoutWebsite>;
+export default DSPPage;
