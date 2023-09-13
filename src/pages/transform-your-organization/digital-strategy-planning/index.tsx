697d021737230a2ad52859e8a0f1bc6b3fb481a2
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-09-13 18:07:27 +0700

    Revert "TYO Thủy 13/09 (#2)"
    This reverts commit 31b6eb89a184b3030caa98ca0ef898a488771ed5.

--- src/pages/transform-your-organization/digital-strategy-planning/index.tsx
+++ src/pages/transform-your-organization/digital-strategy-planning/index.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
 import LayoutWebsite from 'src/shared/components/layout/LayoutWebsite';
 import ChallengeDesignOneSection from '@/components/business/website/TYO&SYB_Common/Challenge_DesignOne_Section';
-import { TYO_DSP_Section3, TYO_DSP_Section4 } from '@/mocks/website/TYO/overview';
-import ServiceDesignThreeSection from '@/components/business/website/TYO&SYB_Common/Service_DesignThree_Section';
-import BannerDesignOneSection from '@/components/business/website/All_Common/Banner_DesignOne_Section';
-import { TYO_DSP_Banner } from '@/mocks/website/TYO/new';
+import { TYOchallenge1 } from '@/mocks/website/TYO/overview';
 
-
-const DSPPage = () => {
+const DTPPage = () => {
   return (
     <React.Fragment>
       <Head>
-        <title>Digital Strategy Planning NGS</title>
-        <meta name='description' content='Digital Strategy Planning NGS' />
+        <title>YNG_ERP NGS</title>
+        <meta name='description' content='YNG_ERP NGS' />
         <meta name='keywords' content='Công nghệ thông tin, Giải pháp số' />
       </Head>
-      <BannerDesignOneSection title="Chuyển đổi số" data={TYO_DSP_Banner}/>
-      <ChallengeDesignOneSection title='Thách thức' data={TYO_DSP_Section3} />
-      <ServiceDesignThreeSection title='Dịch vụ' data={TYO_DSP_Section4} />
+      <ChallengeDesignOneSection title='Thách thức' data={TYOchallenge1} />
     </React.Fragment>
   );
 };
 
-DSPPage.getLayout = (children: React.ReactNode) => <LayoutWebsite>{children}</LayoutWebsite>;
-export default DSPPage;
+DTPPage.getLayout = (children: React.ReactNode) => <LayoutWebsite>{children}</LayoutWebsite>;
+export default DTPPage;
