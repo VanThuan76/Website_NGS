f01a6079aad733e55ae838ec2e57f8d84d613d8f
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-09-14 11:11:33 +0700

    thuanvv/fix-component-v1

--- src/shared/components/business/website/TYO&SYB_Common/Overview_DesignThree_Section/index.tsx
+++ src/shared/components/business/website/TYO&SYB_Common/Overview_DesignThree_Section/index.tsx
@@ -16,13 +16,15 @@ const OverviewDesignThreeSection = ({ title, data, className }: Props) => {
       id={data.section.code}
       className={`pb-4 md:pb-8 lg:pb-16 xl:pb-24 px-4 md:px-24 lg:px-32 xl:px-52 2xl:px-96 overflow-hidden ${className}  `}
     >
-      <div className='relative w-full mx-auto my-auto mt-10 flex justify-between items-start gap-10'>
+      <div className='relative w-full mx-auto my-auto mt-10 flex flex-col justify-between items-center'>
+        <div className='relative w-full flex justify-between items-start gap-10'>
         <div className='w-full flex flex-col justify-start items-start gap-2 z-30'>
           <p className='text-sm md:text-2xl text-orange-500'>{title}</p>
           <h1 className='text-2xl md:text-4xl font-semibold'>{data.section.name}</h1>
         </div>
         <p className='w-full z-30'>{data.section.description}</p>
-        <BorderStyleOneLight className='absolute top-0 left-0 z-10' />
+        </div>
+        <BorderStyleOneLight className='absolute top-0 z-10' />
       </div>
     </section>
   );
