64438e8503e2392ade1357b8848199fe0e1c6058
trinhthuy4801 <trinhthuytt118@gmail.com>
2023-09-15 10:21:46 +0700

    Thủy SYB 15/09

---
+++ src/pages/secure-your-business/threat-intelligence/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Head from 'next/head';
+import LayoutWebsite from 'src/shared/components/layout/LayoutWebsite';
+import BannerDesignOneSection from '@/components/business/website/All_Common/Banner_DesignOne_Section';
+import OverviewDesignFiveSection from '@/components/business/website/TYO&SYB_Common/Overview_DesignFive_Section';
+import ServiceDesignThreeSection from '@/components/business/website/TYO&SYB_Common/Service_DesignThree_Section';
+import WhyUsDesignOneSection from '@/components/business/website/TYO&SYB_Common/WhyUs_DesignOne_Section';
+import PartnerDesignTwoSection from '@/components/business/website/TYO&SYB_Common/Partner_DesignTwo_Section';
+import FormDesignOneSection from '@/components/business/website/TYO&SYB_Common/Form_DesignOne_Section';
+import { SYB_TI_Banner, SYB_TI_Section2, SYB_TI_Section4, SYB_TI_Section5, SYB_TI_Section6, SYB_TI_Section8 } from '@/mocks/website/TYO/thuy_syb';
+
+const TIPage = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Threat Intelligence NGS</title>
+        <meta name='description' content='Threat Intelligence NGS' />
+        <meta name='keywords' content='Công nghệ thông tin, Giải pháp số' />
+      </Head>
+      <BannerDesignOneSection title="Develop new solution" data={SYB_TI_Banner}/>
+      <OverviewDesignFiveSection title='Tổng quan' data={SYB_TI_Section2}/>
+      <ServiceDesignThreeSection title='Dịch vụ' data={SYB_TI_Section4} />
+      <WhyUsDesignOneSection title='Tại sao chọn NGS' data={SYB_TI_Section5}/>
+      <PartnerDesignTwoSection data={SYB_TI_Section6}/>
+      <FormDesignOneSection title='Dùng thử' data={SYB_TI_Section8}/>
+    </React.Fragment>
+  );
+};
+
+TIPage.getLayout = (children: React.ReactNode) => <LayoutWebsite>{children}</LayoutWebsite>;
+export default TIPage;
---
+++ src/shared/mock/website/TYO/thuy_syb.ts
@@ -0,0 +1,375 @@
+export const SYB_TI_Banner = {
+    section: {
+        id: 1,
+        name: 'DỊCH VỤ THÔNG TIN TÌNH BÁO AN NINH MẠNG',
+        code: "PG004.4SE00001",
+        image: "/images/PG004.4SE00001.png",
+        description: "Tăng cường các biện pháp bảo mật để chống lại các mối đe dọa tiềm ẩn đang rình rập trong môi trường của bạn."
+
+    },
+    components: []
+}
+
+export const SYB_TI_Section2 = {
+    section: {
+        id: 2,
+        name: 'Tổng quan về  chuyển đổi số và dịch vụ công nghệ thông tin',
+        code: "PG004.4SE00002",
+        image: "/images/Image_OverviewDesignFIveSection.png",
+        description: "Thông tin về mối đe dọa giúp các tổ chức có thông tin, sau đó xây dựng cơ chế phòng thủ hiệu quả và giảm thiểu rủi ro có thể gây thiệt hại về tài chính và danh tiếng. Thông tin về mối đe dọa là khả năng dự đoán để bảo vệ các cuộc tấn công trong tương lai mà tổ chức có thể gặp phải để họ có thể chủ động điều chỉnh biện pháp phòng thủ của mình và ngăn chặn các cuộc tấn công đó. Tuy nhiên không phải doanh nghiệp nào cũng đủ khả năng làm việc này, chính vì vậy NGS sẽ đồng hành cùng doanh nghiệp của bạn trên con đường an toàn thông tin hiện tại và tương lai."
+
+    },
+    components: [
+    ]
+}
+
+export const SYB_TI_Section4 = {
+    section: {
+        id: 4,
+        name: 'Dịch vụ chúng tôi cung cấp',
+        code: "PG004.4SE00004",
+        image: "",
+        description: "Transform your business with successful strategy"
+
+    },
+    components: [
+        {
+            id: 1,
+            language: 'vi',
+            code: "PG004.4SE00004CN000001",
+            title: "Cung cấp thông tin về dữ liệu bị đánh cắp, rò rỉ",
+            description: "Xác định các lỗ hổng bảo mật và thực hiện khai thác lỗi nhằm đánh giá mức độ ảnh hưởng trên môi trường mạng.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 2,
+            language: 'vi',
+            code: "PG004.4SE00004CN000002",
+            title: " Cung cấp thông tin về các mã độc tấn công có chủ đích (Targeted malware)",
+            description: "Xác định các lỗ hổng bảo mật và thực hiện khai thác lỗi nhằm đánh giá mức độ ảnh hưởng trên các máy chủ.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 3,
+            language: 'vi',
+            code: "PG004.4SE00004CN000003",
+            title: "Cung cấp thông tin về các lỗ hổng an ninh bảo mật",
+            description: "Xác định các lỗ hổng bảo mật và thực hiện khai thác lỗi nhằm đánh giá mức độ ảnh hưởng trên các ứng dụng Web.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 4,
+            language: 'vi',
+            code: "PG004.4SE00004CN000004",
+            title: "Cung cấp thông tin chung về các mối đe dọa, mã độc mới, sự cố về an ninh bảo mật mới xảy ra trên thế giới",
+            description: "Xác định các lỗ hổng bảo mật và thực hiện khai thác lỗi nhằm đánh giá mức độ ảnh hưởng trên các ứng dụng Mobile.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 5,
+            language: 'vi',
+            code: "PG004.4SE00004CN000005",
+            title: "Cung cấp các báo cáo nghiên cứu bảo mật từ đội ngũ chuyên gia của NGS(các lỗ hổng bảo mật 0-day, bài viết phân tích kỹ thuật chuyên sâu,…)",
+            description: "Xác định các lỗ hổng bảo mật và thực hiện khai thác lỗi nhằm đánh giá mức độ ảnh hưởng trên các ứng dụng Mobile.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 6,
+            language: 'vi',
+            code: "PG004.4SE00004CN000006",
+            title: "Hỗ trợ tích hợp với hệ thống SIEM",
+            description: "Xác định các lỗ hổng bảo mật và thực hiện khai thác lỗi nhằm đánh giá mức độ ảnh hưởng trên các ứng dụng Mobile.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 7,
+            language: 'vi',
+            code: "PG004.4SE00004CN000007",
+            title: "Cung cấp tài khoản để truy cập hoặc qua API/STIX/TAXII để tích hợp vào các hệ thống phòng thủ của khách hàng.",
+            description: "Các mối đe đọa sẽ được cung cấp tới khách hàng thông qua giao diện ứng dụng web, khách hàng được cung cấp tài khoản để truy cập hoặc qua API/STIX/TAXII để tích hợp vào các hệ thống phòng thủ của khách hàng.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+    ]
+}
+
+export const SYB_TI_Section5 = {
+    section: {
+        id: 5,
+        name: 'Tại sao nên chọn đồng hành cùng chúng tôi',
+        code: "PG004.4SE00005",
+        image: "",
+        description: "We are trusted partner for your digital transformation:"
+    },
+    components: [
+        {
+            id: 1,
+            language: 'vi',
+            code: "PG004.4SE00005CN000001",
+            title: "Phơi bày các mối đe dọa.",
+            description: "Khám phá hoạt động đe dọa ở bất kỳ ngôn ngữ, định dạng hoặc nền tảng nào theo định kỳ. // Hợp lý hóa quy trình làm việc",
+            slug: "",
+            content: "",
+            image: "/images/PG004.4SE00005.png",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 2,
+            language: 'vi',
+            code: "PG004.4SE00005CN000002",
+            title: "Hợp lý hóa quy trình làm việc",
+            description: "Các lỗ hổng bảo mật được công bố hoặc các dữ liệu nhạy cảm bị rò rỉ liên quan tới khách hàng sẽ được cập nhật và gửi trực tiếp tới đầu mối liên hệ của khách hàng.",
+            slug: "",
+            content: "",
+            image: "/images/PG004.4SE00005.png",
+            video: "",
+            thumbnail: ""
+        },
+    ]
+}
+
+export const SYB_TI_Section6 = {
+    section: {
+        id: 6,
+        name: 'NGS đồng hành cùng những đối tác tuyệt vời',
+        code: "PG004.4SE00006",
+        image: "",
+        description: ""
+
+    },
+    components: [
+        {
+            id: 1,
+            language: 'vi',
+            code: "PG004.4SE00006CN000001",
+            title: "",
+            description: "",
+            slug: "",
+            content: "",
+            image: "/images/logo_fujitsu.png",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 2,
+            language: 'vi',
+            code: "PG004.4SE00006CN000002",
+            title: "",
+            description: "",
+            slug: "",
+            content: "",
+            image: "/images/logo_fis.png",
+            video: "",
+            thumbnail: ""
+        }, {
+            id: 3,
+            language: 'vi',
+            code: "PG004.4SE00006CN000003",
+            title: "",
+            description: "",
+            slug: "",
+            content: "",
+            image: "/images/logo_microsoft.png",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 4,
+            language: 'vi',
+            code: "PG004.4SE00006CN000004",
+            title: "",
+            description: "",
+            slug: "",
+            content: "",
+            image: "/images/logo_vinasoy.png",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 5,
+            language: 'vi',
+            code: "PG004.4SE00006CN000005",
+            title: "",
+            description: "",
+            slug: "",
+            content: "",
+            image: "/images/logo_topica.png",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 6,
+            language: 'vi',
+            code: "PG004.4SE00006CN000006",
+            title: "",
+            description: "",
+            slug: "",
+            content: "",
+            image: "/images/logo_bidv.png",
+            video: "",
+            thumbnail: ""
+        },
+    ]
+}
+
+export const SYB_TI_Section8 = {
+    section: {
+        id: 1,
+        name: 'Bạn muốn đăng ký đề xuất bản dùng thử?',
+        code: "PG004.4SE00008",
+        image: "",
+        description: ""
+    },
+    components: [
+    ]
+}
+
+export const SYB_IR_Banner = {
+    section: {
+        id: 1,
+        name: 'DỊCH VỤ ĐIỀU TRA VÀ ỨNG PHÓ SỰ CỐ AN NINH MẠNG',
+        code: "PG004.5SE00001",
+        image: "/images/PG004.4SE00001.png",
+        description: "Hỗ trợ 24/24 để hỗ trợ quản lý và nâng cao khả năng phản ứng cho doanh nghiệp của bạn trước các sự cố an ninh mạng."
+
+    },
+    components: []
+}
+
+export const SYB_IR_Section2 = {
+    section: {
+        id: 2,
+        name: 'Tổng quan về chuyển đổi số và dịch vụ công nghệ thông tin',
+        code: "PG004.5SE00002",
+        image: "/images/Image_OverviewDesignFIveSection.png",
+        description: "Dịch vụ Điều tra và ứng phó sự cố an ninh mạng nhằm mục đích thực hiện điều tra số – Pháp y kỹ thuật số (Digital Forensic) nhằm xác định các yếu tố liên quan tới sự cố, bao gồm: Các lỗ hổng bảo mật đã bị khai thác; Các hệ thống máy chủ/PC/tài khoản người dùng đã bị ảnh hưởng; Các dữ liệu/thông tin tài khoản bị ảnh hưởng; Các file mã độc, công cụ tấn công đã được sử dụng; Luồng tấn công của attacker. Như vậy việc thực hiện kế hoạch ứng phó là chìa khóa để ngăn chặn hoàn toàn vi phạm cho doanh nghiệp của bạn."
+
+    },
+    components: [
+    ]
+}
+
+export const SYB_IR_Section4 = {
+    section: {
+        id: 4,
+        name: 'Dịch vụ chúng tôi cung cấp',
+        code: "PG004.5SE00004",
+        image: "",
+        description: "Transform your business with successful strategy"
+
+    },
+    components: [
+        {
+            id: 1,
+            language: 'vi',
+            code: "PG004.5SE00004CN000001",
+            title: "Thực hiện quá trình ứng phó sự cố theo từng giai đoạn",
+            description: "Thực hiện quá trình ứng phó sự cố theo từng giai đoạn: Identification (nhận biết); Containment (Ngăn chặn); Remediation (Khắc phục); Recovery (Khôi phục); Lessons Learned (Bài học kinh nghiệm).",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 2,
+            language: 'vi',
+            code: "PG004.5SE00004CN000002",
+            title: "Báo cáo chi tiết về các vấn đề an ninh",
+            description: "Báo cáo chi tiết về các vấn đề an ninh đã phát hiện, có bao gồm các bằng chứng về dấu hiệu đã bị xâm nhập.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 3,
+            language: 'vi',
+            code: "PG004.5SE00004CN000003",
+            title: "Đề xuất phương án khắc phục",
+            description: "Đề xuất phương án khắc phục triệt để hoặc phương án giảm thiểu rủi ro, phương án khắc phục tạm thời đối với các vấn đề an ninh tìm được.",
+            slug: "",
+            content: "",
+            image: "",
+            video: "",
+            thumbnail: ""
+        },
+    ]
+}
+
+export const SYB_IR_Section5 = {
+    section: {
+        id: 5,
+        name: 'Tại sao nên chọn đồng hành cùng chúng tôi',
+        code: "PG004.5SE00005",
+        image: "",
+        description: "We are trusted partner for your digital transformation:"
+    },
+    components: [
+        {
+            id: 1,
+            language: 'vi',
+            code: "PG004.5SE00005CN000001",
+            title: "Công nghệ hàng đầu",
+            description: "Với đội ngũ chuyên gia giàu kinh nghiệm, NGS tự tin cung cấp công nghệ hiện đại hàng đầu cho doanh nghiệp của bạn.",
+            slug: "",
+            content: "",
+            image: "/images/PG004.4SE00005.png",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 2,
+            language: 'vi',
+            code: "PG004.5SE00005CN000002",
+            title: "Xử lý nhanh",
+            description: "Chúng tôi hiểu rằng thời gian là yếu tố cần cam kết ngăn chặn sự cố nên NGS tự tin xử lý nhanh chóng cho tổ chức của bạn.",
+            slug: "",
+            content: "",
+            image: "/images/PG004.4SE00005.png",
+            video: "",
+            thumbnail: ""
+        },
+        {
+            id: 3,
+            language: 'vi',
+            code: "PG004.5SE00005CN000003",
+            title: "Đảm bảo - Chi tiết",
+            description: "Đồng thời chúng tôi khẳng định phương pháp thực hiện và kết quả NGS đưa ra luôn đảm bảo các bước và chi tiết nhất cho doanh nghiệp của bạn",
+            slug: "",
+            content: "",
+            image: "/images/PG004.4SE00005.png",
+            video: "",
+            thumbnail: ""
+        },
+    ]
+}
