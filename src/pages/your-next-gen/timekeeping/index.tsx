a9cf797b3058162895665037f7e9cd140aacc7a3
duonghoang210 <duongph21098@gmail.com>
2023-09-17 11:34:45 +0700

    commit ver 1 YOUR NEXT GEN

---
+++ src/pages/your-next-gen/timekeeping/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import LayoutWebsite from 'src/shared/components/layout/LayoutWebsite';
+import YNGCommonPilotSubriceSection from '@/components/business/website/YNG/YNG_Common/YNG_Common_PilotSubrice_Section';
+import YNGDetailWhyChooseSection from '@/components/business/website/YNG/YNG_Detail/YNG_Detail_WhyChoose_Section';
+import YNGDetailProductSection from '@/components/business/website/YNG/YNG_Detail/YNG_Detail_Product_Section';
+import YNGDetailDirectionSetion from '@/components/business/website/YNG/YNG_Detail/YNG_Detail_Direction_Section';
+import YNGCommonCustomerSection from '@/components/business/website/YNG/YNG_Common/YNG_Common_Customer_Section';
+import YNGCommonBackSolutionSection from '@/components/business/website/YNG/YNG_Common/YNG_Common_BackSolution_Section';
+import YNGCommonIntroductionSection from '@/components/business/website/YNG/YNG_Common/YNG_Common_Introduction_Section';
+import { YNGCommonBackSolution1, YNGCommonCustomer1, YNGCommonIntroduction1, YNGCommonPilotSubrice1, YNGDetailBanner1, YNGDetailDirection1, YNGDetailProduct1, YNGDetailWhyChoose1 } from '@/mocks/website/YNG/YNG_Detail/example';
+import YNGDetailBannerSection from '@/components/business/website/YNG/YNG_Detail/YNG_Detail_Banner_Section';
+import { YNG_Timekeeping_Section2, YNG_Timekeeping_Section3, YNG_Timekeeping_Section4, YNG_Timekeeping_Section5, YNG_Timekeeping_Section6, YNG_Timekeeping_Section7, YNG_Timekeeping_Section8 } from '@/mocks/website/TYO/duong_yng';
+
+const ScrollRevealWrapper = dynamic(() => import('@/components/common/customization/ScrollRevealWrapper'), {
+  ssr: false,
+});
+const TimekeepingDetailPage = () => (
+  <React.Fragment>
+    <Head>
+      <title>Chấm công</title>
+      <meta name='description' content='Chấm công' />
+      <meta name='keywords' content='Công nghệ thông tin, Giải pháp số' />
+    </Head>
+    <YNGDetailBannerSection data={YNGDetailBanner1} />
+    <YNGDetailWhyChooseSection title='Tại sao chọn Chấm công?' data={YNG_Timekeeping_Section2} />
+    <YNGDetailProductSection data={YNG_Timekeeping_Section3} />
+    <YNGDetailDirectionSetion data={YNG_Timekeeping_Section4} />
+    <YNGCommonCustomerSection title='Khách hàng' data={YNG_Timekeeping_Section5} />
+    <YNGCommonIntroductionSection data={YNG_Timekeeping_Section6} />
+    <YNGCommonBackSolutionSection title='Back Solution' data={YNG_Timekeeping_Section7} />
+    <YNGCommonPilotSubriceSection title='Pilot' data={YNG_Timekeeping_Section8} />
+  </React.Fragment>
+);
+
+TimekeepingDetailPage.getLayout = (children: React.ReactNode) => <LayoutWebsite>{children}</LayoutWebsite>;
+export default TimekeepingDetailPage;
