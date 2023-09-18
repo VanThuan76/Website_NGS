653de60b0f9cea3fad15a848778466b0c3a3885e
duonghoang210 <duongph21098@gmail.com>
2023-09-18 15:37:17 +0700

    [Dương] fix refactor syb

--- src/pages/secure-your-business/compromise-assessment/index.tsx
+++ src/pages/secure-your-business/compromise-assessment/index.tsx
@@ -5,10 +5,10 @@ import BannerDesignOneSection from '@/components/business/website/All_Common/Ban
 import OverviewDesignSixSection from '@/components/business/website/TYO&SYB_Common/Overview_DesignSix_Section';
 import WhyUsDesignOneSection from '@/components/business/website/TYO&SYB_Common/WhyUs_DesignOne_Section';
 import PartnerDesignTwoSection from '@/components/business/website/TYO&SYB_Common/Partner_DesignTwo_Section';
-import { TYOBanner1_PG4_3, TYOForm1_PG4_3, TYOOverview6_PG4_3, TYOPartner2_PG4_3, TYOService5_PG4_3, TYOWhyUs1_PG4_3, TYOclientStories1_PG4_3 } from '@/mocks/website/TYO/duong';
 import ServiceDesignFiveSection from '@/components/business/website/TYO&SYB_Common/Service_DesignFive_Section';
 import ClientStoriesDesignOneSection from '@/components/business/website/TYO&SYB_Common/ClientStories_DesignOne_Section';
 import FormDesignOneSection from '@/components/business/website/TYO&SYB_Common/Form_DesignOne_Section';
+import { SYBBanner1_PG4_3, SYBForm1_PG4_3, SYBOverview6_PG4_3, SYBPartner2_PG4_3, SYBService5_PG4_3, SYBWhyUs1_PG4_3, SYBclientStories1_PG4_3 } from '@/mocks/website/SYB/syb';
 
 const SOCPage = () => {
   return (
@@ -18,13 +18,13 @@ const SOCPage = () => {
         <meta name='description' content='YNG_ERP NGS' />
         <meta name='keywords' content='Công nghệ thông tin, Giải pháp số' />
       </Head>
-      <BannerDesignOneSection title="Develop new solution" data={TYOBanner1_PG4_3}/>
-      <OverviewDesignSixSection title='Tổng quan' data={TYOOverview6_PG4_3} />
-      <ServiceDesignFiveSection title='NGS triển khai' data={TYOService5_PG4_3} />
-      <WhyUsDesignOneSection title='Tại sao chọn NGS' data={TYOWhyUs1_PG4_3}/>
-      <PartnerDesignTwoSection data={TYOPartner2_PG4_3}/>
-      <ClientStoriesDesignOneSection title='' data={TYOclientStories1_PG4_3} />
-      <FormDesignOneSection title='Dùng thử' data={TYOForm1_PG4_3} />
+      <BannerDesignOneSection title="Develop new solution" data={SYBBanner1_PG4_3}/>
+      <OverviewDesignSixSection title='Tổng quan' data={SYBOverview6_PG4_3} />
+      <ServiceDesignFiveSection title='NGS triển khai' data={SYBService5_PG4_3} />
+      <WhyUsDesignOneSection title='Tại sao chọn NGS' data={SYBWhyUs1_PG4_3}/>
+      <PartnerDesignTwoSection data={SYBPartner2_PG4_3}/>
+      <ClientStoriesDesignOneSection title='' data={SYBclientStories1_PG4_3} />
+      <FormDesignOneSection title='Dùng thử' data={SYBForm1_PG4_3} />
 
     </React.Fragment>
   );
