fad3cc6e55280824a047c3834e51f69277f6090b
trinhthuy4801 <109933291+trinhthuy4801@users.noreply.github.com>
2023-09-20 01:24:07 +0700

    Thủy fix component [2] (#24)
    * [Dương] fix refactor syb (#20)
    
    * [Dương] fix refactor syb
    
    * update
    
    * Delete duong_yng copy.ts
    
    * Thủy fix component [1] (#21)
    
    * [Dương] Super Refactor (#22)
    
    * [Dương] Fix component after refactor (#23)
    
    * Thủy fix component [2]
    
    ---------
    
    Co-authored-by: duonghoang210 <46072991+Dg0210@users.noreply.github.com>

--- src/shared/components/business/website/TYO&SYB_Common/Overview_DesignThree_Section/index.tsx
+++ src/shared/components/business/website/TYO&SYB_Common/Overview_DesignThree_Section/index.tsx
@@ -15,18 +15,18 @@ const OverviewDesignThreeSection = ({ title, data, className }: Props) => {
   return (
     <section
       id={data.section.code}
-      className={`pb-4 md:pb-8 lg:pb-16 xl:pb-24 px-4 md:px-24 lg:px-32 xl:px-52 2xl:px-96 overflow-hidden ${className}  `}
+      className={`pb-4 md:pb-8 lg:pb-16 xl:pb-24 px-4 md:p-24 lg:px-32 xl:px-52 2xl:px-96 overflow-hidden ${className}  `}
     >
       <div className='relative w-full mx-auto my-auto mt-10 flex flex-col justify-between items-center'>
-        <div className='relative w-full flex justify-between items-start gap-10'>
-          <div className='w-full flex flex-col justify-start items-start gap-2 z-30'>
+        <div className='relative w-[1440px] flex justify-between items-start gap-10'>
+          <div className='flex flex-col justify-start items-start gap-4 z-30 w-[392px]'>
             <p className='text-sm md:text-2xl text-orange-500'>{title}</p>
-            <h1 className='text-2xl md:text-4xl font-medium'>{data.section.name}</h1>
+            <h1 className='text-2xl md:text-[40px] leading-[48px] font-medium'>{data.section.name}</h1>
           </div>
-          <div className='w-full flex flex-col z-30'>
+          <div className='flex flex-col z-30 gap-9 w-[604px]'>
             {splitTextToArrayByDoubleSlash(data.section.description).map((item: string, idx: number) => (
-              <div key={idx} className='flex flex-col gap-4 pb-4'>
-                <p>{item}</p>
+              <div key={idx} className='flex flex-col pb-4'>
+                <p className='font-normal text-base'>{item}</p>
               </div>
             ))}
           </div>
--- src/shared/components/business/website/TYO&SYB_Common/Service_DesignThree_Section/index.tsx
+++ src/shared/components/business/website/TYO&SYB_Common/Service_DesignThree_Section/index.tsx
@@ -13,7 +13,7 @@ const ServiceDesignThreeSection = ({ title, data, className }: Props) => {
   if (!data || !data.components || !data.section) return <React.Fragment></React.Fragment>;
 
   return (
-    <section id={data.section.code} className={`pb-4 px-4 md:px-24 lg:px-32 overflow-hidden ${className}  `}>
+    <section id={data.section.code} className={`pb-4 px-4 md:p-24 lg:px-32 overflow-hidden ${className}  `}>
       <div className='w-full mx-auto my-auto mt-10 grid grid-cols-2 justify-between items-start gap-10'>
         <div className='lg:mt-10 w-full max-h-[500px] pr-4 overflow-hidden overflow-y-scroll flex flex-col justify-start items-start gap-10 service-designthree-section-with-scrollbar'>
           {data.components.map((item, idx) => {
@@ -27,7 +27,7 @@ const ServiceDesignThreeSection = ({ title, data, className }: Props) => {
         <div className='w-full h-full flex flex-col justify-start items-end'>
           <div className='flex flex-col justify-end items-end gap-3'>
             <p className='text-sm md:text-2xl'>{title}</p>
-            <h1 className='text-2xl md:text-4xl font-semibold'>{data.section.name}</h1>
+            <h1 className='text-2xl md:text-4xl font-semibold text-end'>{data.section.name}</h1>
             <p className='mt-5 font-thin'>{data.section.description}</p>
           </div>
           <div className='mt-10 flex flex-col justify-end items-end gap-3'>
