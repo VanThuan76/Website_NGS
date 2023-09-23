f67c8052a1ea0c57bfa3a1d1b2bbbfdc44d4e9fa
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-09-23 09:25:14 +0700

    thuanvv/fix_build

--- src/shared/components/business/website/HO/HO_Security_Section/HOSecurityResponsive.tsx
+++ src/shared/components/business/website/HO/HO_Security_Section/HOSecurityResponsive.tsx
@@ -7,7 +7,7 @@ const HOSecurityResponsive = ({data}: Props) => {
   return (
     <div className='max-w-[1440px] w-full my-auto flex lg:hidden flex-col justify-between items-center lg:items-end gap-5 mt-10 mx-auto'>
       <div className='grid grid-cols-2 justify-end items-end gap-2 md:grid-cols-3 md:gap-3'>
-        {data.components!.map((item , index) => <HomeSecurityCard title={item.title} image={item.image}  key={index} className='col-span-1' /> )}
+        {data.components!.map((item , index) => <HomeSecurityCard title={item.title} description={item.description} image={item.image}  key={index} className='col-span-1' /> )}
 
       </div>
     </div>
